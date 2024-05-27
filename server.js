023e5990ff60d106170e71e99fbc845082415b54
arielhernandezcl <ariel.dox@gmail.com>
2024-05-27 02:54:07 -0400

    minimal fix

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const joyasRoutes = require('./routes/joyas');
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use('/joyas', joyasRoutes);
 
-app.listen(PORT, () => {
-   console.log(`Servidor funcionando en el puerto ${PORT}`);
+app.listen(port, () => {
+   console.log(`Servidor funcionando en el puerto ${port}`);
 });
